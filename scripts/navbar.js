f9e6a5e62a05f72dc14fe473d3f0802ec4981db4
c4mpos-dev <campos.eet@gmail.com>
2024-07-14 18:33:52 -0300

    Scroll adjustment

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,7 +11,21 @@ function toggleMenu() {
 /* rolagem suave */
 function smoothScroll(event, targetId) {
     event.preventDefault();
-    document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
+    var target = document.querySelector(targetId);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var duration = 800; //  controla o tempod e rolagem (ms)
+    var start = null;
+
+    window.requestAnimationFrame(function step(timestamp) {
+        if (!start) start = timestamp;
+        var progress = timestamp - start;
+        var percent = Math.min(progress / duration, 1); // percentagem concluída
+        window.scrollTo(0, startPosition + distance * percent);
+        if (progress < duration) {
+            window.requestAnimationFrame(step);
+        }
     });
 }
+
