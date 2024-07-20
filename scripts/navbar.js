90c609bd62ab8b632636dc0a35e52dc53f9cd0bc
c4mpos-dev <campos.eet@gmail.com>
2024-07-19 22:20:54 -0300

    About finished and fix scroll

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -10,16 +10,16 @@ function toggleMenu() {
 function smoothScroll(event, targetId) {
     event.preventDefault();
     var target = document.querySelector(targetId);
-    var targetPosition = target.offsetTop;
+    var targetPosition = target.offsetTop - 80; // Subtrai 80px do topo da seção
     var startPosition = window.pageYOffset;
     var distance = targetPosition - startPosition;
-    var duration = 800; // duração da rolagem em milissegundos
+    var duration = 800; // Duração da rolagem em milissegundos
     var start = null;
 
     window.requestAnimationFrame(function step(timestamp) {
         if (!start) start = timestamp;
         var progress = timestamp - start;
-        var percent = Math.min(progress / duration, 1); // percentagem concluída
+        var percent = Math.min(progress / duration, 1); // Percentagem concluída
         window.scrollTo(0, startPosition + distance * percent);
         if (progress < duration) {
             window.requestAnimationFrame(step);
