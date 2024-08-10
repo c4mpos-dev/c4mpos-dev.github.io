6cd6da9275c733bd1193d8fe2f3f1fabe67dd799
c4mpos-dev <campos.eet@gmail.com>
2024-08-09 22:35:04 -0300

    Back-end email finished

--- scripts/email.js
+++ scripts/email.js
@@ -3,9 +3,24 @@
     emailjs.init("-hjOau9iUZtxNFkXG"); // Substitua YOUR_USER_ID pelo seu user ID do EmailJS
 })();
 
+function showNotification(message, type) {
+    notification.textContent = message;
+    notification.className = `notification ${type}`;
+    notification.style.transition = 'transform 0.5s ease';
+    notification.style.transform = 'scale(1)';
+
+    setTimeout(function() {
+        notification.classList.remove('show');
+        notification.style.transition = 'transform 0.5s ease';
+        notification.style.transform = 'scale(0)';
+    }, 2000); 
+    
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('email-form');
     var loadingOverlay = document.getElementById('loading');
+    var notification = document.getElementById('notification');
 
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Evita o comportamento padrão de envio do formulário
@@ -15,15 +30,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
         emailjs.sendForm('service_73zdw4j', 'template_wf9dp3x', form)
             .then(function() {
-                console.log('E-mail enviado com sucesso!');
-                alert('E-mail enviado com sucesso!');
+                showNotification('E-mail enviado com sucesso!', 'success');
                 loadingOverlay.style.display = 'none'; // Oculta a tela de loading
                 document.body.classList.remove('no-scroll'); // Libera o scroll
             }, function(error) {
-                console.log('Erro ao enviar e-mail:', error);
-                alert('Erro ao enviar e-mail, tente novamente.');
+                showNotification('Erro ao enviar e-mail, tente novamente.', 'error');
                 loadingOverlay.style.display = 'none'; // Oculta a tela de loading
                 document.body.classList.remove('no-scroll'); // Libera o scroll
             });
     });
+
 });
