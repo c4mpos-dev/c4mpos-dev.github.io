87f446b78a9743732d8d3e84ec9432353f41dc09
c4mpos-dev <campos.eet@gmail.com>
2024-08-17 00:06:41 -0300

    BugFix

--- scripts/email.js
+++ scripts/email.js
@@ -1,7 +1,5 @@
-const config = require('./config'); // Importa o arquivo de configuração
-
 (function(){
-    emailjs.init("-hjOau9iUZtxNFkXG"); // Substitua YOUR_USER_ID pelo seu user ID do EmailJS
+    emailjs.init("-hjOau9iUZtxNFkXG");
 })();
 
 function showNotification(message, type) {
@@ -17,28 +15,28 @@ function showNotification(message, type) {
         notification.style.transition = 'transform 0.5s ease';
         notification.style.transform = 'scale(0)';
     }, 2000); 
-
+    
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('email-form');
     var loadingOverlay = document.getElementById('loading');
-    
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Evita o comportamento padrão de envio do formulário
-	
-        document.addEventListener('DOMContentLoaded', function() {
 
-            emailjs.sendForm('service_73zdw4j', 'template_wf9dp3x', form)
-                .then(function() {
-                    showNotification('E-mail enviado com sucesso!', 'success');
-                    loadingOverlay.style.display = 'none'; // Oculta a tela de loading
-                    document.body.classList.remove('no-scroll'); // Libera o scroll
-                }, function(error) {
-                    showNotification('Erro ao enviar e-mail, tente novamente.', 'error');
-                    loadingOverlay.style.display = 'none'; // Oculta a tela de loading
-                    document.body.classList.remove('no-scroll'); // Libera o scroll
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        loadingOverlay.style.display = 'block'; 
+        document.body.classList.add('no-scroll'); 
+
+        emailjs.sendForm('service_73zdw4j', 'template_wf9dp3x', form)
+            .then(function() {
+                showNotification('E-mail enviado com sucesso!', 'success');
+                loadingOverlay.style.display = 'none'; 
+                document.body.classList.remove('no-scroll'); 
+            }, function(error) {
+                showNotification('Erro ao enviar e-mail, tente novamente.', 'error');
+                loadingOverlay.style.display = 'none'; 
+                document.body.classList.remove('no-scroll'); 
             });
-        }); 
     });
 });
