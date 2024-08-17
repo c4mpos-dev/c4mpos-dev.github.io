ca27833acbb3faf60dd0c015b382356131295ff5
c4mpos-dev <campos.eet@gmail.com>
2024-08-16 23:57:03 -0300

    Remove index.js

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID
+};
--- scripts/email.js
+++ scripts/email.js
@@ -1,3 +1,9 @@
+const config = require('./config'); // Importa o arquivo de configuração
+
+(function(){
+    emailjs.init(config.EMAILJS_SERVICE_ID); // Substitua YOUR_USER_ID pelo seu user ID do EmailJS
+})();
+
 function showNotification(message, type) {
     var notification = document.getElementById('notification');
 
@@ -11,41 +17,28 @@ function showNotification(message, type) {
         notification.style.transition = 'transform 0.5s ease';
         notification.style.transform = 'scale(0)';
     }, 2000); 
+
 }
 
-
-var form = document.getElementById('email-form');
-var loadingOverlay = document.getElementById('loading');
-
-
-document.getElementById('email-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    loadingOverlay.style.display = 'block';
-    document.body.classList.add('no-scroll');
-
-    const formData = new FormData(this);
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('email-form');
+    var loadingOverlay = document.getElementById('loading');
     
-    fetch('/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: formData.get('name'),
-            subject: formData.get('subject'),
-            content: formData.get('content')
-        })
-    })
-    .then(response => response.text())
-    .then(data => {
-        showNotification('E-mail enviado com sucesso!', 'success');
-        loadingOverlay.style.display = 'none';
-        document.body.classList.remove('no-scroll');
-    })
-    .catch(error => {
-        showNotification('Erro ao enviar e-mail, tente novamente.', 'error');
-        loadingOverlay.style.display = 'none';
-        document.body.classList.remove('no-scroll');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+	
+        document.addEventListener('DOMContentLoaded', function() {
+
+            emailjs.sendForm(config.EMAILJS_SERVICE_ID, config.EMAILJS_TEMPLATE_ID, form)
+                .then(function() {
+                    showNotification('E-mail enviado com sucesso!', 'success');
+                    loadingOverlay.style.display = 'none'; // Oculta a tela de loading
+                    document.body.classList.remove('no-scroll'); // Libera o scroll
+                }, function(error) {
+                    showNotification('Erro ao enviar e-mail, tente novamente.', 'error');
+                    loadingOverlay.style.display = 'none'; // Oculta a tela de loading
+                    document.body.classList.remove('no-scroll'); // Libera o scroll
+            });
+        }); 
     });
 });
